a94b9c5230cfeb2e5996d0660668f73c351d514f
wangfeiping <wangfeiping@outlook.com>
2020-06-09 11:52:25 +0800

    implements proxy

--- cmd/weeder/proxy.go
+++ cmd/weeder/proxy.go
@@ -1,29 +1,35 @@
 package main
 
 import (
+	"net/http"
+	"runtime"
+	"strconv"
+
+	"github.com/wangfeiping/weeder/log"
+	"github.com/wangfeiping/weeder/server"
 	"github.com/wangfeiping/weeder/util"
 )
 
 var (
-// mTimeout = 10 //seconds
-// mMaxCpu  = 0  //maximum number of CPUs. 0 means all available CPUs
+	mTimeout = 10 //seconds
+	mMaxCpu  = 0  //maximum number of CPUs. 0 means all available CPUs
 )
 
 func serv(config *util.WeederConfig) bool {
-	// if mMaxCpu < 1 {
-	// 	mMaxCpu = runtime.NumCPU()
-	// }
-	// runtime.GOMAXPROCS(mMaxCpu)
+	if mMaxCpu < 1 {
+		mMaxCpu = runtime.NumCPU()
+	}
+	runtime.GOMAXPROCS(mMaxCpu)
 
-	// listeningAddress := config.Ip + ":" + strconv.Itoa(config.Port)
+	listeningAddress := config.Ip + ":" + strconv.Itoa(config.Port)
 
-	// log.DebugS("main", "config: version ", util.VERSION)
+	log.DebugS("main", "config: version ", util.VERSION)
 
-	// _ = server.NewProxyServer(config)
-	// e := http.ListenAndServe(listeningAddress, nil)
-	// if e != nil {
-	// 	log.ErrorS("main", "startup error: ", e)
-	// 	return false
-	// }
+	_ = server.NewProxyServer(config)
+	e := http.ListenAndServe(listeningAddress, nil)
+	if e != nil {
+		log.ErrorS("main", "startup error: ", e)
+		return false
+	}
 	return true
 }
